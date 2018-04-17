bfa7e47dc99fb09e1cad1b58c3a32b047f7fcc94
Oliver-wys <oliverw1006@qq.com>
2018-04-17 22:44:50 +0800

    将pom中SpringMVC所需要的依赖配齐，将必要的jar包放入lib文件夹中，可以运行

--- src/qbs/AppConfig.java
+++ src/qbs/AppConfig.java
@@ -3,6 +3,7 @@ package qbs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -11,22 +12,23 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@ComponentScan("main.java.qbs")
+@ComponentScan("qbs")
 @EnableWebMvc
+
 public class AppConfig extends WebMvcConfigurationSupport{
 
 	@Bean
 	public ViewResolver viewResolver(){
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
-		viewResolver.setPrefix("webapp/WEB-INF/jsp");
+		viewResolver.setPrefix("/WEB-INF/jsp/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/assets/**").addResourceLocations("/main/webapp/public/");
+		registry.addResourceHandler("/assets/**").addResourceLocations("/public/");
 	}
 	
 }
---
+++ src/qbs/dao/IUserDao.java
@@ -0,0 +1,5 @@
+package qbs.dao;
+
+public interface IUserDao {
+
+}
