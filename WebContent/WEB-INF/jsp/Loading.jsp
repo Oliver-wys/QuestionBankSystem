827b016b15e950814b5305099f0ba940ca88841c
Oliver-wys <oliverw1006@qq.com>
2018-04-17 20:37:15 +0800

    调整回webcontent

--- WebContent/WEB-INF/jsp/Loading.jsp
+++ WebContent/WEB-INF/jsp/Loading.jsp
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<script type="text/javascript" src="/QuestionBankSystem/src/main/webapp/public/js/jquery.js"></script>
+<!-- <script type="text/javascript" src="/QuestionBankSystem/asset/js/jquery.js"></script> -->
 <title>Loading Page</title>
 </head>
 <body>
---
+++ src/main/java/qbs/AppConfig.java
@@ -0,0 +1,32 @@
+package qbs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan("main.java.qbs")
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurationSupport{
+
+	@Bean
+	public ViewResolver viewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("webapp/WEB-INF/jsp");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**").addResourceLocations("/main/webapp/public/");
+	}
+	
+}
